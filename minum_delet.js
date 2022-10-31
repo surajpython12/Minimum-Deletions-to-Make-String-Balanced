1d894b149d48f56445395cab0ac865e719df64af
suraj_singh_90 <96347310+surajpython12@users.noreply.github.com>
2022-10-31 13:25:38 +0530

    Add files via upload

---
+++ minum_delet.js
@@ -0,0 +1,16 @@
+var minimumDeletions = function (s) {
+    let res = [];
+    let count = 0;
+    for (let idx = s.length - 1; idx >= 0; idx--) {
+      if (res.length == 0) {
+        res.push(s[idx]);
+      } else if (res[res.length - 1] == "a" && s[idx] == "b") {
+        count += 1;
+        res.pop();
+      } else {
+        res.push(s[idx]); 
+      }
+    }
+    return count;
+  };
+console.log(minimumDeletions("aababbab"))
